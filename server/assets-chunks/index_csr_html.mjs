8542e1a815bbfe8ef34f57bc128c44d834ab2b2b
hieupham <hieupham@safeforweb.com>
2025-03-29 03:04:29 +0700

    Auto-generated commit

--- server/assets-chunks/index_csr_html.mjs
+++ server/assets-chunks/index_csr_html.mjs
@@ -1,9 +1,9 @@
-<!doctype html>
+export default `<!doctype html>
 <html lang="en" data-beasties-container="">
 <head>
   <meta charset="utf-8">
   <title>Portfolio</title>
-  <base href="/portfolio">
+  <base href="/">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
 <style>body{font-family:Arial,sans-serif;margin:0;padding:0;background:#0d1b2a;color:#fff}</style><link rel="stylesheet" href="styles-7UVFABKS.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="styles-7UVFABKS.css"></noscript></head>
@@ -11,3 +11,4 @@
   <app-root></app-root>
 <script src="polyfills-FFHMD2TL.js" type="module"></script><script src="main-W4VPMUSE.js" type="module"></script></body>
 </html>
+`;
